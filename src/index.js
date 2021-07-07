e98b19b4a5b2fe958792920cd079843822ef437e
simpleistruth <45241193+simpleistruth@users.noreply.github.com>
2021-07-07 14:08:12 -0700

    took out misc loggers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import Game from './scripts/game'
 document.addEventListener("DOMContentLoaded", () => {
   const game = new Game();
   game.createCanvas();
-  console.log('hello123234')
   document.addEventListener('keypress', (e) => {if (e.code === 'Space') { game.launch()}});
 });
